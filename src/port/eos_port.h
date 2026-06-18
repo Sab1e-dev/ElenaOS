a725abd04cc848baa0df3a51cb551c5e50b50dad
Sab1e-dev <sab1e@qq.com>
2026-06-19 00:29:45 +0800

    fix: prevent __GNUC__ branch matching MinGW, which defines both macros

--- src/port/eos_port.h
+++ src/port/eos_port.h
@@ -26,7 +26,7 @@ extern "C" {
     #define EOS_WEAK __weak
 #elif defined(__IAR_SYSTEMS_ICC__) /* for IAR Compiler */
     #define EOS_WEAK __weak
-#elif defined(__GNUC__) /* GNU GCC Compiler */
+#elif defined(__GNUC__) && !defined(__MINGW32__) /* GNU GCC Compiler */
     #define EOS_WEAK __attribute__((weak))
 #elif defined(__ADSPBLACKFIN__) /* for VisualDSP++ Compiler */
     #define EOS_WEAK __attribute__((weak))
